20b2448f00b20ef17011d6c02c6f94017279600f
amangupta2509 <amangupta.t6@gmail.com>
2026-03-02 13:35:05 +0530

    booking.tsx

--- components/themed-text.tsx
+++ components/themed-text.tsx
@@ -2,7 +2,14 @@ import { StyleSheet, Text, type TextProps } from 'react-native';
 
 import { useThemeColor } from '@/hooks/use-theme-color';
 
-nst color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
+export type ThemedTextProps = TextProps & {
+  lightColor?: string;
+  darkColor?: string;
+
+  type = 'default',
+  ...rest
+}: ThemedTextProps) {
+  const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
 
   return (
     <Text
