3af21ac3e324ab141c55c537594128db2364a68d
amangupta2509 <amangupta.t6@gmail.com>
2026-03-02 13:34:49 +0530

    booking.tsx

--- components/themed-text.tsx
+++ components/themed-text.tsx
@@ -2,7 +2,23 @@ import { StyleSheet, Text, type TextProps } from 'react-native';
 
 import { useThemeColor } from '@/hooks/use-theme-color';
 
+nst color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
 
+  return (
+    <Text
+      style={[
+        { color },
+        type === 'default' ? styles.default : undefined,
+        type === 'title' ? styles.title : undefined,
+        type === 'defaultSemiBold' ? styles.defaultSemiBold : undefined,
+        type === 'subtitle' ? styles.subtitle : undefined,
+        type === 'link' ? styles.link : undefined,
+        style,
+      ]}
+      {...rest}
+    />
+  );
+}
 
 const styles = StyleSheet.create({
   default: {
