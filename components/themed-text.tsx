ce856dbdb215722084ad06dab8d97913a39b2714
amangupta2509 <amangupta.t6@gmail.com>
2026-03-02 13:35:17 +0530

    booking.tsx

--- components/themed-text.tsx
+++ components/themed-text.tsx
@@ -5,7 +5,13 @@ import { useThemeColor } from '@/hooks/use-theme-color';
 export type ThemedTextProps = TextProps & {
   lightColor?: string;
   darkColor?: string;
+  type?: 'default' | 'title' | 'defaultSemiBold' | 'subtitle' | 'link';
+};
 
+export function ThemedText({
+  style,
+  lightColor,
+  darkColor,
   type = 'default',
   ...rest
 }: ThemedTextProps) {
