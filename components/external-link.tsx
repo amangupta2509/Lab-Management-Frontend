79981850b710fe2e8ea0d72e6f4659e03a829d84
amangupta2509 <amangupta.t6@gmail.com>
2026-03-09 16:33:39 +0530

    external link

--- components/external-link.tsx
+++ components/external-link.tsx
@@ -1,4 +1,10 @@
-e link in an in-app browser.
+
+      href={href}
+      onPress={async (event) => {
+        if (process.env.EXPO_OS !== 'web') {
+          // Prevent the default behavior of linking to the default browser on native.
+          event.preventDefault();
+          // Open the link in an in-app browser.
           await openBrowserAsync(href, {
             presentationStyle: WebBrowserPresentationStyle.AUTOMATIC,
           });
