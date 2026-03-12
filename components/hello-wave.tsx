d9d0a036ba37ff26f0634c1d8b0911612da104f2
amangupta2509 <amangupta.t6@gmail.com>
2026-03-12 16:38:37 +0530

    heloo wave.tsx

--- components/hello-wave.tsx
+++ components/hello-wave.tsx
@@ -4,7 +4,11 @@ export function HelloWave() {
   return (
     <Animated.Text
       style={{
-   { transform: [{ rotate: "25deg" }] },
+        fontSize: 28,
+        lineHeight: 32,
+        marginTop: -6,
+        animationName: {
+          "50%": { transform: [{ rotate: "25deg" }] },
         },
         animationIterationCount: 4,
         animationDuration: "300ms",
