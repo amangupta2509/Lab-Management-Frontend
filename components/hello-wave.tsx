eed811291e919cae667804dd18e5d91a32557664
amangupta2509 <amangupta.t6@gmail.com>
2026-03-12 16:38:19 +0530

    heloo wave.tsx

--- components/hello-wave.tsx
+++ components/hello-wave.tsx
@@ -1,2 +1,14 @@
 import Animated from "react-native-reanimated";
 
+export function HelloWave() {
+  return (
+    <Animated.Text
+      style={{
+   { transform: [{ rotate: "25deg" }] },
+        },
+        animationIterationCount: 4,
+        animationDuration: "300ms",
+      }}
+    ></Animated.Text>
+  );
+}
