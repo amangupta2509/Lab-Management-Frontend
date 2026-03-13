96d6fb0da291ea0fbfdfae111e244ea902bb258d
amangupta2509 <amangupta.t6@gmail.com>
2026-03-13 23:54:54 +0530

    haptic tab tsx

--- components/themed-view.tsx
+++ components/themed-view.tsx
@@ -1,7 +1,14 @@
 import { View, type ViewProps } from 'react-native';
 
 import { useThemeColor } from '@/hooks/use-theme-color';
-roundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
+
+export type ThemedViewProps = ViewProps & {
+  lightColor?: string;
+  darkColor?: string;
+};
+
+export function ThemedView({ style, lightColor, darkColor, ...otherProps }: ThemedViewProps) {
+  const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
 
   return <View style={[{ backgroundColor }, style]} {...otherProps} />;
 }
