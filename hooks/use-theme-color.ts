3942028ee184c5bedd97e2b546345b74e8c10971
amangupta2509 <amangupta.t6@gmail.com>
2026-03-26 23:11:24 +0530

    use color scheme web

--- hooks/use-theme-color.ts
+++ hooks/use-theme-color.ts
@@ -6,5 +6,8 @@
   import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
-
+export function useThemeColor(
+  props: { light?: string; dark?: string },
+  colorName: keyof typeof Colors.light & keyof typeof Colors.dark
+) {
 }
