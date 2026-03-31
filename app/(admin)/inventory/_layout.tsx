ee1dd7b0919e9b967c4568892b49f19d02a6571c
amangupta2509 <amangupta.t6@gmail.com>
2026-03-31 17:24:00 +0530

    inventory _layout.tsx

--- app/(admin)/inventory/_layout.tsx
+++ app/(admin)/inventory/_layout.tsx
@@ -7,7 +7,11 @@ export default function InventoryLayout() {
         headerShown: false,
       }}
     >
- />
+      <Stack.Screen name="index" />
+      <Stack.Screen name="projects" />
+      <Stack.Screen name="run-plans" />
+      <Stack.Screen name="transactions" />
+      <Stack.Screen name="alerts" />
       <Stack.Screen name="consumption" />
     </Stack>
   );
